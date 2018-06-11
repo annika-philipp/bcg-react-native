cba41acb10328ef3df9e88c97b73ccf2798e03a0
Annika <liebste.anni@gmail.com>
2018-06-11 13:14:15 +1200

    adds location reducer

--- redux/reducers/location.js
+++ redux/reducers/location.js
@@ -1,10 +1,10 @@
-// const location = (state = 'welcome', action) => {
-//   switch (action.type) {
-//     case 'NAVIGATE':
-//       return action.destination
-//     default:
-//       return state
-//   }
-// }
+const location = (state = 'welcome', action) => {
+  switch (action.type) {
+    case 'NAVIGATE':
+      return action.destination
+    default:
+      return state
+  }
+}
 
-// export default location
+export default location
