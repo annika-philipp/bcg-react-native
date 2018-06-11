0823ffc909b90979394903c7f69073e5b1678029
Annika <liebste.anni@gmail.com>
2018-06-11 13:14:51 +1200

    adds questionsIndex reducer

--- redux/reducers/questionsIndex.js
+++ redux/reducers/questionsIndex.js
@@ -1,12 +1,12 @@
-// const index = (state = 0, action) => {
-//   switch (action.type) {
-//     case 'INCREASE_INDEX':
-//       return action.num + 1
-//     case 'RESET':
-//       return 0
-//     default:
-//       return state
-//   }
-// }
+const index = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREASE_INDEX':
+      return action.num + 1
+    case 'RESET':
+      return 0
+    default:
+      return state
+  }
+}
 
-// export default index
+export default index
