7fa801d002493c51d1960867b30d1ef1770f069c
Annika <liebste.anni@gmail.com>
2018-06-14 12:08:38 +1200

    takes out console logs

--- api/api.js
+++ api/api.js
@@ -1,26 +1,20 @@
 import request from 'superagent'
 import { get } from './localstorage'
 
-const baseURL = 'https://bcg-redux.herokuapp.com/api/'
+const baseURL = 'https://bcg-redux.herokuapp.com/'
 
 export default function consume(method = 'get', endpoint, data = {}) {
   const dataMethod = method.toLowerCase() === 'get' && 'query' || 'send'
-
-  return get('questions').then(token => {
     const headers = {
       Accept: 'application/json'
     }
-
-    console.log(baseURL + endpoint)
-
     return request[method](baseURL + endpoint)
     .set(headers)[dataMethod](data)
     .then((res) => {
       return res
     })
     .catch(err => {
-      console.log('Hello', err)
+      console.log('Hello I have an error', err)
       throw err
     })
-  })
 }
